332c05dd0185d5f1ca26b70eb7df09cb2ce536b8
qus0in <qus0in@gmail.com>
2025-03-11 15:20:43 +0900

    merge

--- src/main/java/org/example/ex11/controller/PrivateController.java
+++ src/main/java/org/example/ex11/controller/PrivateController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class PrivateController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/private.jsp")
-                .forward(req, resp);
+
+        req.getRequestDispatcher("/WEB-INF/private.jsp").forward(req, resp);
     }
 }
